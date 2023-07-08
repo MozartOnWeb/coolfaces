0737e41f6fedf3af0f1cd51c2f01a12507ff494f
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-07-08 19:07:55 +0000

    fixing nav error

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = (props: Props) => {
   };
 
   return (
-    <div className="navbar">
+    <div className="navbar" onClick={(e) => e.stopPropagation()}>
       <Link href={""} className="logo">
         CF
       </Link>
