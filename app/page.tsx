7e73837bd5ce98d526df297852f58fa611f6927d
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-07-08 19:48:54 +0000

    working on typeface components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+//import components
+import { Typeface } from "@/components/typeface/typeface";
+
 export default function Home() {
   return (
     <main className="container">
       <h1>All typefaces</h1>
+
+      <section className="typefaces-container">
+        <Typeface />
+        <Typeface />
+        <Typeface />
+      </section>
     </main>
   );
 }
---
+++ components/typeface/typeface.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export const Typeface = (props: Props) => {
+  return <div className="typeface">typeface</div>;
+};
