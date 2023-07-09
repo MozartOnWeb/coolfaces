4c22d6573fb2cfcd661892614e32ee97c32b62af
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-07-09 15:20:09 +0000

    single-typeface page is done

--- app/typeface/[slug]/page.tsx
+++ app/typeface/[slug]/page.tsx
@@ -13,67 +13,61 @@ import TypefaceTitle from "@/assets/typeface-page.svg";
 
 export default function Page({ params }: { params: { slug: string } }) {
   return (
-    <main className="container">
-      <section className="typeface-page">
-        <Image
-          className="typeface-page__svg"
-          src={TypefaceTitle}
-          alt="typeface"
-        />
+    <section className="typeface-page">
+      <Image
+        className="typeface-page__svg"
+        src={TypefaceTitle}
+        alt="typeface"
+      />
 
-        <section className="typeface-infos">
-          {/* LEFT SIDE */}
-          <div className="left">
-            <div className="part">
-              <h4>Category</h4>
-              <p>Display</p>
-            </div>
-
-            <Separator />
-
-            <div className="part">
-              <h4>Styles</h4>
-              <p>1 style</p>
-            </div>
-
-            <Separator />
-
-            <div className="part">
-              <h4>License</h4>
-              <p>SIL Open Font License, Version 1.1</p>
-            </div>
-
-            <Separator />
-
-            <div className="part">
-              <h4>Infos</h4>
-              <p>
-                Pilowlava (sic) was born as an intuitive, fast-paced creative
-                feedback loop in which its creators tried to surprise one
-                another. The result is a typeface that recalls cooled lava flows
-                drawn with a compass.
-              </p>
-            </div>
-
-            <Separator />
-            <div className="download">
-              <p>
-                Get this typeface <Download />
-              </p>
-            </div>
+      <section className="typeface-infos">
+        {/* LEFT SIDE */}
+        <div className="left">
+          <div className="part">
+            <h4>Category</h4>
+            <p>Display</p>
           </div>
 
-          {/* RIGHT SIDE */}
-          <div className="right">
-            <Image className="typeface-icon" src={TypefaceID} alt="typeface" />
-            <Image
-              className="typeface-background-img"
-              src={Img}
-              alt="typeface"
-            />
+          <Separator />
+
+          <div className="part">
+            <h4>Styles</h4>
+            <p>1 style</p>
           </div>
-        </section>
+
+          <Separator />
+
+          <div className="part">
+            <h4>License</h4>
+            <p>SIL Open Font License, Version 1.1</p>
+          </div>
+
+          <Separator />
+
+          <div className="part">
+            <h4>Infos</h4>
+            <p>
+              Pilowlava (sic) was born as an intuitive, fast-paced creative
+              feedback loop in which its creators tried to surprise one another.
+              The result is a typeface that recalls cooled lava flows drawn with
+              a compass.
+            </p>
+          </div>
+
+          <Separator />
+          <div className="download">
+            <p>
+              Get this typeface <Download />
+            </p>
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="right">
+          <Image className="typeface-icon" src={TypefaceID} alt="typeface" />
+          <Image className="typeface-background-img" src={Img} alt="typeface" />
+        </div>
       </section>
-    </main>
+    </section>
   );
 }
---
+++ app/typeface/layout.tsx
@@ -0,0 +1,25 @@
+//import components
+import { Typeface } from "@/components/typeface/typeface";
+
+export default function TypefaceLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="container typeface-layout">
+      {children}
+      <div className="typeface-layout-latest">
+        <h1>Latest typefaces</h1>
+
+        <section className="typefaces-container">
+          <Typeface />
+          <Typeface />
+          <Typeface />
+          <Typeface />
+          <Typeface />
+        </section>
+      </div>
+    </section>
+  );
+}
