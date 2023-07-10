ebf1d7820f225eba1c2fb74063811df4bdff6000
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-07-10 23:15:21 +0000

    create components

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -18,7 +18,21 @@ export default async function Home() {
 
       <section className="typefaces-container">
         {typefaces.map((typeface) => (
-          <Suspense key={typeface.slug} fallback={<p>Loading feed...</p>}>
+          <Suspense
+            key={typeface.slug}
+            fallback={
+              <p
+                style={{
+                  textAlign: "center",
+                  backgroundColor: "red",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                Loading feed...
+              </p>
+            }
+          >
             <Typeface
               slug={typeface.slug}
               alt={typeface.name}
