94d7503fab519c7cb61262c5562d072769c2ffa4
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-07-10 00:50:04 +0000

    typings

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,21 +1,23 @@
 //import components
+import { Typeface as TypefaceTypes } from "@/typings";
+
 import { Typeface } from "@/components/typeface/typeface";
 import { Pagination } from "@/components/pagination/pagination";
 
-export default function Home() {
+//import fetch function
+import { getAllTypefaces } from "@/sanity/fetch";
+
+export default async function Home() {
+  const typefaces: TypefaceTypes[] = await getAllTypefaces();
+
   return (
     <main className="container">
       <h1>All typefaces</h1>
 
       <section className="typefaces-container">
-        <Typeface />
-        <Typeface />
-        <Typeface />
-        <Typeface />
-        <Typeface />
-        <Typeface />
-        <Typeface />
-        <Typeface />
+        {typefaces.map((typeface) => (
+          <Typeface key={typeface.slug} />
+        ))}
       </section>
 
       <Pagination />
--- sanity/fetch.ts
+++ sanity/fetch.ts
@@ -6,14 +6,17 @@ export const getAllTypefaces = async () => {
   return sanityClient.fetch(groq`
     *[_type == "typeface" && !(_id in path("drafts.**"))] {
             name,
+              infos,
+              "files": files.asset -> url,
+              styles,
             "slug": slug.current,
             "background": background.asset -> url,
             "icon": icon.asset -> url,
               categories[] -> {
-                name
+                name,
             },
               license[] -> {
-                name
+                name,
             }
         }
     `);
